cff41f6430af67d2d97b066a3935ffdb934dcc8d
Reza141106 <d032410372@student.utem.edu.my>
2026-05-24 00:31:46 +0800

    Rename database.sql - Reza

---
+++ config/mail.php
@@ -0,0 +1,4 @@
+<?php
+define('MAIL_USER', 'usiirs.utem@gmail.com');  // ← your project Gmail
+define('MAIL_PASS', 'vmlz pqhp msdo yyxl');         // ← your 16-char App Password, no spaces
+define('MAIL_FROM_NAME', 'U-SIIRS System');
